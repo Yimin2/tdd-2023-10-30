e77b1a4cc4511aad3b69ade8924b2a367ce585ea
Yimin2 <gownfla14@naver.com>
2023-10-30 09:49:26 +0900

    Calc class

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class Calc {
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+        System.out.printf(cmd);
+    }
+}
