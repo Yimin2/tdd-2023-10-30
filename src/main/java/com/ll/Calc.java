355fa85af5efed877bb07bd9852f1f94eec376bb
Yimin2 <gownfla14@naver.com>
2023-10-30 14:05:33 +0900

    */ update

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,11 +2,23 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
-      if ( exp.contains("-")){
-          return 0;
-      }
-      else {
-          return 20;
-      }
+        if (exp.isBlank()) return 0;
+        final String[] expBits = exp.split(" ");
+        final String sign = expBits[1];
+        final int num1 = Integer.parseInt(expBits[0]);
+        final int num2 = Integer.parseInt(expBits[2]);
+
+        final int rs = switch (sign) {
+            case "+":
+                yield num1 + num2;
+            case "-":
+                yield num1 - num2;
+            case "*":
+                yield num1 * num2;
+            default:
+                yield num1 / num2;
+        };
+        return rs;
     }
 }
+
